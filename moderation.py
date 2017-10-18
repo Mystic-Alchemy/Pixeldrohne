2386341c2abfa7efa28efae59b7e67ce8c975a36
PilleniusMC <pilleniusmc@gmail.com>
2017-10-18 19:32:06 +0200

    Ein paar neue Sachen Änderungen: + Anti Invite + invite.txt mit zufälligen Sprüchen für den Anti Invite * License und Code of conduct verschoben in die Docs

--- moderation.py
+++ moderation.py
@@ -2,6 +2,7 @@ import sqlite3
 import discord
 import sys
 import keys
+import random
 
 client = discord.Client()
 connection = sqlite3.connect("bot.db")
@@ -27,13 +28,31 @@ async def on_message(message):
         await client.send_message(message.channel, "Es ist ein Fehler aufgetreten: {e}".format(e=e))
 
     # Mod hinzufügen
-    if message.author.id == (pmcid or pxlid or rpyid) and message.content.lower().startswith('p.madd'):
+    if message.author.id == (keys.pmcid or keys.pxlid) and message.content.lower().startswith('p.madd'):
         user = message.mentions[0]
         mod = user.id
         madd = open("mods.txt", "a", encoding='utf-8')
         madd.write('"{0]"'.format(mod) + "\n")
         await client.send_message(message.channel, "{0} wurde erfolgreich den Mods hinzugefügt".format(user.name))
         madd.close()
+
+    # Anti invite
+    if ('discord'.lower() in message.content.lower()) and (message.author.id not in keys.exceptions):
+        if 'discord.me'.lower() in message.content.lower():
+            user = message.author
+            aif = open("config/invite.txt", "r", encoding='utf-8')
+            ainv = aif.readlines()
+            anti = random.choice(ainv)
+            await client.delete_message(message)
+            await client.send_message(message.channel, anti + " " + user.mention)
+        if 'discord.gg'.lower() in message.content.lower():
+            user = message.author
+            aif = open("config/invite.txt", "r", encoding='utf-8')
+            ainv = aif.readlines()
+            anti = random.choice(ainv)
+            await client.delete_message(message)
+            await client.send_message(message.channel, anti + " " + user.mention)
+
 
     # Botstop
     if message.content.lower().startswith('p.halt') and message.author.id == keys.pmcid:
