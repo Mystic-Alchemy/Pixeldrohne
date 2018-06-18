de073a004685a40849304c3d99a076cc14afd6ef
PilleniusMC <pilleniusmc@gmail.com>
2018-06-18 15:26:49 +0200

    Moderation Ban hinzugefügt

--- moderation.py
+++ moderation.py
@@ -30,3 +30,27 @@ class Mods:
             await ctx.send('Nutzer konnte nicht gekicked werden.')
         if isinstance(error, commands.MissingPermissions):
             await ctx.send('Sorry, du kannst diesen Befehl leider nicht nutzen.')
+
+    @commands.command(name="ban", no_pm=True)
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, user: discord.Member, *arg):
+        if arg == ():
+            reason = "Der Nutzer wurde vom Server geworfen."
+        else:
+            reason = " ".join(arg)
+        embed = discord.Embed(
+            title="Kick",
+            description=f"Der Nutzer {user.name} wurde mit dem Grund `{reason}` gebannt.",
+            color=self.color
+        )
+        await discord.Guild.kick(ctx.guild, user=user, reason=reason)
+        await ctx.channel.send(embed=embed)
+
+    @ban.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('Es schaut fast so aus als sei der Nutzer nicht mehr auf dem Server.')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Nutzer konnte nicht gebannt werden.')
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Sorry, du kannst diesen Befehl leider nicht nutzen.')
